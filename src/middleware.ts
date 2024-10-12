397ce166c3c491ffe60c6432aab46e10c0dafe4e
wpahmedali <wp.ahmedali@gmail.com>
2024-10-13 01:56:25 +0500

    update route name

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,18 +5,18 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   if (!currentUser) {
-    if (pathname === '/login' || pathname === '/register') {
+    if (pathname === '/login' || pathname === '/signup') {
       return NextResponse.next();
     } else {
       return NextResponse.redirect(new URL('/login', request.url));
     }
   }
 
-  if (currentUser && (pathname === '/login' || pathname === '/register')) {
+  if (currentUser && (pathname === '/login' || pathname === '/signup')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 }
 
 export const config = {
-  matcher: ['/', '/login', '/register'],
+  matcher: ['/', '/login', '/signup'],
 };
