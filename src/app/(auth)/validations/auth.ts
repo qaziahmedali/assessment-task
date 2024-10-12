683ad5588523f6ef36540d347c4a76895a2d5d74
wpahmedali <wp.ahmedali@gmail.com>
2024-10-13 01:46:21 +0500

    pull

--- src/app/(auth)/validations/auth.ts
+++ src/app/(auth)/validations/auth.ts
@@ -5,7 +5,7 @@ const commonValidations = {
     .email('Invalid email address')
     .required('Email is required'),
   password: Yup.string()
-    .min(6, 'Password must be at least 6 characters long')
+    .min(8, 'Password must be at least 6 characters long')
     .required('Password is required'),
 };
 
